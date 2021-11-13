dd64349d7ed8de7a579e6a4cceee26ce27199e8a
Ktsue6 <ktsue808@hawaii.edu>
2021-11-13 13:52:19 -1000

    Assignment done

---
+++ Tsue_Kameron_Assignment1/products.js
@@ -0,0 +1,38 @@
+// Master products
+products = [
+    // Product 1
+    product 1 = {
+        "model":"Apple iPhone 13 Pro Max",
+        "price":1100,
+        "image":'./images/iP13PM.jpeg',
+        "quantity available":6
+    },
+    // Product 2
+    product 2 = {
+        "model":"Google Pixel 6 Pro",
+        "price":900,
+        "image":'./images/GP6P.jpeg',
+        "quantity available":4
+    },
+    // Product 3
+    product 3 = {
+        "model":"Samsung Galaxy S21 Ultra",
+        "price":1200,
+        "image":'./images/SGS21U.jpeg',
+        "quantity available":5
+    },
+    // Product 4
+    product 4 = {
+        "model":"Oneplus 9 Pro",
+        "price":1070,
+        "image":'./images/OP9P.jpeg',
+        "quantity available":3
+    },
+    // Product 5
+    product 5 = {
+        "model":"Sony Xperia Pro-i",
+        "price":1800,
+        "image":'./images/SXPI.jpeg',
+        "quantity available":2
+    }
+]
