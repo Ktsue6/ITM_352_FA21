f218a3c72adef4955fba83a9a224df055aba7e02
Ktsue6 <ktsue808@hawaii.edu>
2021-11-20 23:51:14 -1000

    Assignment 1 done

--- Tsue_Kameron_Assignment1/products.js
+++ Tsue_Kameron_Assignment1/products.js
@@ -1,38 +1,40 @@
 // Master products
-products = [
-    // Product 1
-    product 1 = {
-        "model":"Apple iPhone 13 Pro Max",
-        "price":1100,
-        "image":'./images/iP13PM.jpeg',
-        "quantity available":6
-    },
-    // Product 2
-    product 2 = {
-        "model":"Google Pixel 6 Pro",
-        "price":900,
-        "image":'./images/GP6P.jpeg',
-        "quantity available":4
-    },
-    // Product 3
-    product 3 = {
-        "model":"Samsung Galaxy S21 Ultra",
-        "price":1200,
-        "image":'./images/SGS21U.jpeg',
-        "quantity available":5
-    },
-    // Product 4
-    product 4 = {
-        "model":"Oneplus 9 Pro",
-        "price":1070,
-        "image":'./images/OP9P.jpeg',
-        "quantity available":3
-    },
-    // Product 5
-    product 5 = {
-        "model":"Sony Xperia Pro-i",
-        "price":1800,
-        "image":'./images/SXPI.jpeg',
-        "quantity available":2
+[
+
+    {  
+        "name":"Apple iPhone 13 Pro Max",  
+        "price": 1100,  
+        "image": "./images/iP13PM.jpeg",
+        "weight": 320,
+        "quantity_available": 500
+    }, 
+    {  
+        "name":"Google Pixel 6 Pro",  
+        "price": 900,  
+        "image": "./images/GP6P.jpeg",
+        "weight": 7,
+        "quantity_available": 250
+    }, 
+    {  
+        "name":"Samsung Galaxy S21 Ultra",  
+        "price": 1200,  
+        "image": "./images/SGS21U.jpeg",
+        "weight": 3.5,
+        "quantity_available": 150
+    }, 
+    {  
+        "name":"Oneplus 9 Pro",  
+        "price": 1070,  
+        "image": "./images/OP9P.jpeg",
+        "weight": 15,
+        "quantity_available": 100
+    }, 
+    {  
+        "name":"Sony Xperia Pro-i",  
+        "price": 1800,  
+        "image": "./images/SXPI.jpeg",
+        "weight": 0.5,
+        "quantity_available": 30
     }
+
 ]
